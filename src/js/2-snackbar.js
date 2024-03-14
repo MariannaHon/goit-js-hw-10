813aba0e8bcc9eaa1ac446444a5a3d40530767fa
MariannaHon <imarianna96i@gmail.com>
2024-03-14 23:12:18 +0100

    commit-2
    finished =)

---
+++ src/js/2-snackbar.js
@@ -0,0 +1,48 @@
+import iziToast from "izitoast";
+import "izitoast/dist/css/iziToast.min.css";
+
+const form = document.querySelector('.form');
+
+form.addEventListener('submit', e => {
+  e.preventDefault();
+
+  const delay = e.currentTarget.elements.delay.value;
+  const option = e.currentTarget.elements.state.value;
+
+  const promise = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (option === "fulfilled") {
+        resolve(displayToastGood(`✅ Fulfilled promise in ${delay}ms`));
+      }
+      else {
+        reject(displayToast(`❌ Rejected promise in ${delay}ms`));
+      }
+    }, delay);
+  });
+
+  promise
+    .then(() => {
+      console.log('`✅ Fulfilled promise in ${delay}ms`')
+    })
+    .catch(() => {
+      console.log('`❌ Rejected promise in ${delay}ms`')
+    });
+});
+
+function displayToast(message) {
+  iziToast.error({
+    title: 'Error',
+    message: message,
+    position: 'topRight',
+  });
+}
+
+function displayToastGood(message) {
+  iziToast.show({
+    title: 'Ok',
+    backgroundColor: 'green',
+    message: message,
+    position: 'topRight',
+  });
+}
+
